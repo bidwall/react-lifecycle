215c129bbb3e9783f0aa69d3ec1d4fe749aba0a5
Palvinder Bidwall <pal@bidwall.co.uk>
2017-05-20 11:46:38 +0100

    Renamed App.js to App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from '../logo.svg';
 import './App.css';
 
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/components/App.jsx</code> and save to reload.
         </p>
       </div>
     );
