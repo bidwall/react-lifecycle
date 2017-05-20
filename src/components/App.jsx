9c4a5533c72008fc8de6617617fa258b558b11a1
Palvinder Bidwall <pal@bidwall.co.uk>
2017-05-20 13:36:59 +0100

    Add update lifecyle methods

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,9 +7,20 @@ class App extends Component {
     super();
     console.log("constructor - call super (pass props if any) and set state");
   }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    //let React know if a component's output is not affected by the current change in state or props
+    console.log("shouldComponentUpdate - invoked before rendering when new props or state are being received");
+    //returning false does not prevent child components from re-rendering when their state changes
+    return true;
+  }
   
   componentWillMount() {
     console.log("componentWillMount - component WILL be rendered to the DOM");
+  }
+
+  componentWillUpdate() {
+    console.log("componentWillUpdate -  invoked immediately before rendering when new props or state are being received");
   }
 
   render() {
@@ -31,7 +42,19 @@ class App extends Component {
   componentDidMount() {
     console.log("componentDidMount - component HAS been rendered to the DOM");
   }
+
+
+
+  componentWillReceiveProps(nexpProps) {
+    console.log("componentWillReceiveProps - invoked before a mounted component receives new props");
+  }
+
   
+
+  componentDidUpdate() {
+    console.log("componentDidUpdate - invoked immediately after updating occurs");
+  }
+
 }
 
 export default App;
