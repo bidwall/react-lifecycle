df9ca7bd1894471c877e813fc72e79c9683a5511
Palvinder Bidwall <pal@bidwall.co.uk>
2017-05-29 09:26:18 +0100

    Create display component

---
+++ src/components/display/Display.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Display extends React.Component {
+    render() {
+        <div>
+            Display something
+        </div>
+    }
+}
+
+export default Display;
