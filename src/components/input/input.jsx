93be559dae657ee27c2e7608b62e787a23e803e9
Palvinder Bidwall <pal@bidwall.co.uk>
2017-05-29 12:16:26 +0100

    Add controls and events to input control

--- src/components/input/input.jsx
+++ src/components/input/input.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
 
 class Input extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { name: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        console.log(`Input value: ${event.target.value}`);
+        this.setState({ name: event.target.value});
+    }
+
+    handleSubmit(event) {
+        alert(`Submitting: ${this.state.name}`);
+        event.preventDefault();
+    }
+
     render() {
         return (
             <div>
-                Input something
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Name:
+                        <input type="text" value={this.state.name} onChange={this.handleChange} />
+                    </label>
+                    <input type="submit" value="Submit" />
+                </form>
             </div>
         );        
     }
